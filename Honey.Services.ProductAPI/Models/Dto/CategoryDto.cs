292c5b6db6eb972f31d86707abb1e3d6f564e0b5
Vantruongg <toiiuvn013@gmail.com>
2021-12-16 12:01:47 +0700

    create dto

---
+++ Honey.Services.ProductAPI/Models/Dto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Honey.Services.ProductAPI.Models.Dto
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Honey.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Honey.Services.ProductAPI.Models.Dto
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryDto Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
