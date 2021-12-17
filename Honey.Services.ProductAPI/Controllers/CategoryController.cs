302838891cdb4a4ce820d78464a88e2b61f760f8
Vantruongg <toiiuvn013@gmail.com>
2021-12-17 17:52:15 +0700

    CRUD category, product

---
+++ Honey.Services.ProductAPI/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Honey.Services.ProductAPI.Models.Dto;
+using Honey.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Honey.Services.ProductAPI.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitofwork;
+        protected ResponseDto _response;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitofwork = unitOfWork;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> GetCategories()
+        {
+            try
+            {
+                IEnumerable<CategoryDto> categoriesInDb = await _unitofwork.Category.GetCategories();
+                _response.Result = categoriesInDb;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{categoryId}")]
+        public async Task<object> GetCategories(int categoryId)
+        {
+            try
+            {
+                CategoryDto categoryInDb = await _unitofwork.Category.GetCategoryById(categoryId);
+                _response.Result = categoryInDb;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> CreateCategory(CategoryDto categoryRequest)
+        {
+            try
+            {
+                CategoryDto categoryInDb = await _unitofwork.Category.CreateUpdateCategory(categoryRequest);
+                _response.Result = categoryInDb;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> UpdateCatgory(CategoryDto categoryRequest)
+        {
+            try
+            {
+                CategoryDto categoryInDb = await _unitofwork.Category.CreateUpdateCategory(categoryRequest);
+                _response.Result = categoryInDb;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{categoryId}")]
+        public async Task<object> DeleteCategory(int categoryId)
+        {
+            try
+            {
+                bool categoryDeleted = await _unitofwork.Category.DeleteCategory(categoryId);
+                _response.Result = categoryDeleted;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+    }
+}
---
+++ Honey.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using Honey.Services.ProductAPI.Models.Dto;
+using Honey.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Honey.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        protected ResponseDto _response;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> GetProducts()
+        {
+            try
+            {
+                IEnumerable<ProductDto> listProductInDb = await _unitOfWork.Product.GetProducts();
+                _response.Result = listProductInDb;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<object> GetProduct(int productId)
+        {
+            try
+            {
+                ProductDto productInDb = await _unitOfWork.Product.GetProductById(productId);
+                _response.Result = productInDb;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> CreateProduct(ProductDto productRequest)
+        {
+            try
+            {
+                ProductDto productInDb = await _unitOfWork.Product.CreateUpdateProduct(productRequest);
+                _response.Result = productInDb;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> UpdateProduct(ProductDto productRequest)
+        {
+            try
+            {
+                ProductDto productInDb = await _unitOfWork.Product.CreateUpdateProduct(productRequest);
+                _response.Result = productInDb;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<object> DeleteProduct(int productId)
+        {
+            try
+            {
+                bool productIsDeleted = await _unitOfWork.Product.DeleteProduct(productId);
+                _response.Result = productIsDeleted;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+    }
+}
