5958641457412c34f212ba18d99fa1ca93c5ad28
Vantruongg <toiiuvn013@gmail.com>
2022-01-18 13:25:29 +0700

    add entity model for shopping cart service

---
+++ Honey.Services.ShoppingCartAPI/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Honey.Services.ShoppingCartAPI.Model
+{
+    public class Product
+    {
+        // Not auto generate productId
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
