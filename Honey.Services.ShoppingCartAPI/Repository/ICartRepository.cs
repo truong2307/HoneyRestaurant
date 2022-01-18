d5b512ed444b442ad53c5e6b2427813d18cf3420
Vantruongg <toiiuvn013@gmail.com>
2022-01-18 19:19:00 +0700

    add interface ICartrepository

---
+++ Honey.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Honey.Services.ShoppingCartAPI.Model.Dto;
+using System.Threading.Tasks;
+
+namespace Honey.Services.ShoppingCartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartDto> GetCartById(string userId);
+        Task<CartDto> CreateUpdateCart(CartDto cartRequest);
+        Task<bool> RemoveFromCart(int cartDetailsId);
+        Task<bool> ClearCart(string userId);
+    }
+}
