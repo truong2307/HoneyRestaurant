a392781f5029d83ecbdc5b69b6fdfe10670a2007
Vantruongg <toiiuvn013@gmail.com>
2022-02-11 16:35:07 +0700

    add icartservice

---
+++ Honey.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using Honey.Web.Models.Dto;
+using System.Threading.Tasks;
+
+namespace Honey.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<T> GetCartByUserIdAsync<T>(string userId, string token = null);
+        Task<T> AddToCartAsync<T>(CartDto cartRequest, string token = null);
+        Task<T> UpdateCartAsync<T>(CartDto cartRequest, string token = null);
+        Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
+    }
+}
